b77de77d109097072735a62561b1f424a6a99936
kweku360 <kweku360@gmail.com>
2015-08-25 07:24:23 +0000

    =commit 2

--- app/router.js
+++ app/router.js
@@ -6,6 +6,31 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('logins',{path : '/'});
+
+  this.resource('suits',function(){
+    this.route('new');
+    this.route('plaintiffs');
+    this.route('defendants');
+    this.route('entersuit');
+    this.route('addlawyer');
+  });
+  this.resource('documents',function(){
+    this.route('entersuit');
+    this.route('detail');
+    this.route('upload');
+  });
+
+  this.resource('judges',function(){
+    this.route('addjudge');
+    this.route('manage');
+
+  });
+  this.resource('lawyers',function(){
+    this.route('addlawyer');
+    this.route('manage');
+
+  });
 });
 
 export default Router;
